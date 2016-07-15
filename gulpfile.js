0c8ffbef99496e02976050ddebd56399e1f31191
Brad West <brad@bradonomics.com>
2016-07-15 14:26:07 -0600

    Updated gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ var gulp        = require('gulp'),
 
 
 //* Run Jekyll build and serve commands
-gulp.task('build', shell.task(['jekyll build --config _config-dev.yml --incremental --watch']));
+gulp.task('build', shell.task(['jekyll build --config "_config.yml,_config-dev.yml" --incremental --watch']));
+// gulp.task('build', shell.task(['jekyll build --config "_config.yml,_config-dev.yml" --watch']));
 
 
 //* BrowserSync
@@ -14,11 +15,11 @@ gulp.task('browser-sync', function () {
       baseDir: '_site/'
     },
     browser: 'firefox',
-    reloadDelay: 900
+    reloadDelay: 300
   });
   gulp.watch('_site/**/*.*').on('change', browserSync.reload);
 });
 
 
-// Default task (build and serve)
+//* Default task (build and serve)
 gulp.task('default', ['build', 'browser-sync']);
